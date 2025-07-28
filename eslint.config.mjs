5ede1ba5ec303357a3d0e66974d13a9b00482e38
Justin Hollingsworth <jwhollingsworth@gmail.com>
2025-07-28 12:37:39 -0400

    ignore a bunch of rules for now

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,4 +9,16 @@ export default tseslint.config(eslint.configs.recommended, tseslint.configs.reco
       tsconfigRootDir: import.meta.dirname,
     },
   },
+  rules: {
+    '@typescript-eslint/await-thenable': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    'no-prototype-builtins': 'off',
+  },
 });
